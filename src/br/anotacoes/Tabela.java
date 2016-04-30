ff3d653b12b80eb659658e6b8c83e5238ecde9d5
Paulo Henrique <p.h.moreirabarbosa@hotmail.com>
2016-04-30 01:14:17 -0300

    Interface Dao Criada e Classe SqlGen

--- src/br/anotacoes/Tabela.java
+++ src/br/anotacoes/Tabela.java
@@ -5,5 +5,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Tabela {
-	String value() default "";
+ String value() default "";
 }
---
+++ src/br/interfaceseclasses/Cliente.java
@@ -0,0 +1,5 @@
+package br.interfaceseclasses;
+
+public class Cliente {
+
+}
